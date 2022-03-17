a1e6f72b47c65300418b6d90226a86b41726046c
devikakushwah <95033933+devikakushwah@users.noreply.github.com>
2022-03-17 15:38:48 +0530

    changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended:true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname,"public")));
 app.use("/admin",adminRouter);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port,()=>{
     console.log("server running....");
 });
