71d5790d5d5e6c84e63f5e9ffcfc2a6a63e5bd1f
devikakushwah <95033933+devikakushwah@users.noreply.github.com>
2022-03-17 22:49:28 +0530

    users

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const adminRouter = require('./routes/admin.route');
 const categoryRouter = require('./routes/category.route');
+const userRouter = require('./routes/user.route');
 const app = express();
 mongoose.connect("mongodb+srv://devikakushwah:Radhakrishna%4029@newcluster.7o13k.mongodb.net/jobBydevika");
 
 app.use(express.static(path.join(__dirname,"public")));
 app.use(bodyParser.urlencoded({ extended:true}));
 app.use(bodyParser.json());
+app.use("/user",userRouter);
 app.use("/admin",adminRouter);
 app.use("/category",categoryRouter);
 const port = process.env.PORT || 8080;
