aa47e688fde2bc38c5a78574b7e1ffa12b6e7e28
devikakushwah <95033933+devikakushwah@users.noreply.github.com>
2022-03-17 23:30:45 +0530

    users

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,28 +1,30 @@
 const express = require('express');
 const User = require('../model/user.model');
-const nodemailer = require('nodemailer');
+var  nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 const router = express.Router();
 
 router.post('/signup',(request, response) => {
     User.create({name:request.body.name, email:request.body.email
     ,password:request.body.password}).then(result=>{
       console.log(result);
-      const email = request.body.email;
-      var transporter = nodemailer.createTransport({
-       service: 'gmail',
-       auth: {
-         user: 'devikakushwah29@gmail.com',
-         pass: 'Database@29'
-       }
-     });
-   
+      
+      var transporter = nodemailer.createTransport(smtpTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        auth: {
+            user: 'devikakushwah29@gmail.com',
+            pass: 'Database@29'
+         }
+      }));
+           
      var mailOptions = {
-       from: 'devikakushwah29@gmail.com',
-       to: email,
-       subject: 'checking ',
-       text: 'devika sending email for project!'
-     };
-   
+        from: 'devikakushwah29@gmail.com',
+        to: 'devikakushwah29@gmail.com',
+        subject: 'checking ',
+        text: 'devika sending email for project!'
+      };
+
      transporter.sendMail(mailOptions, function (error, info) {
        if (error) {
          console.log(error);
