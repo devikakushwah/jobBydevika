cdbbeae0313f3596f0544d59d767d857cd20ce68
devikakushwah <95033933+devikakushwah@users.noreply.github.com>
2022-03-17 23:00:35 +0530

    users

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -3,15 +3,12 @@ const  Admin  = require('../model/admin.model');
 const router = express.Router();
 
 router.post('/login',(request,response)=>{
-    console.log(request.body);
   Admin.findOne({
       email: request.body.email,
       password: request.body.password
   }).then(result=>{
-      console.log(result);
       return response.status(200).json(result);
   }).catch(err=>{
-      console.log(err);
       return response.status(500).json({msg:"something happened.."})
   });
 });
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const User = require('../model/user.model');
+const nodemailer = require('nodemailer');
 const router = express.Router();
 
 router.post('/signup',(request, response) => {
     User.create({name:request.body.name, email:request.body.email
     ,password:request.body.password}).then(result=>{
       console.log(result);
-      return response.status(200).json(result);
+      const email = request.body.email;
+      var transporter = nodemailer.createTransport({
+       service: 'gmail',
+       auth: {
+         user: 'devikakushwah29@gmail.com',
+         pass: 'Database@29'
+       }
+     });
+   
+     var mailOptions = {
+       from: 'devikakushwah29@gmail.com',
+       to: email,
+       subject: 'checking ',
+       text: 'devika sending email for project!'
+     };
+   
+     transporter.sendMail(mailOptions, function (error, info) {
+       if (error) {
+         console.log(error);
+         return response.status(500).json({msg:"Error logged"});
+       } else {
+        console.log('Email sent: ' + info.response);
+        return response.status(200).json(result);
+        
+       }
+     });
+  
+     
     }).catch(err=>{
         console.log(err);
      return response.status(500).json({msg:"Error logged"});
