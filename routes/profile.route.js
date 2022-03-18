fff88764e6e730de5b4f9410f2af6794d776241c
devikakushwah <95033933+devikakushwah@users.noreply.github.com>
2022-03-18 17:02:24 +0530

    user again

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Profile = require('../model/profile.model');
 const auth = require('../middleware/auth');
 const User = require('../model/user.model');
+const {check,validationResult} = require('express-validator');
 const router = express.Router();
 
 router.get('/me',auth,async(request,response,next) => {
@@ -16,68 +17,7 @@ router.get('/me',auth,async(request,response,next) => {
    return response.status(500).json({message: 'error'});
  }
 });
-router.post(
-  '/',
-  auth,
-  check('status', 'Status is required').notEmpty(),
-  check('skills', 'Skills is required').notEmpty(),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
 
-    // destructure the request
-    const {
-      website,
-      skills,
-      youtube,
-      twitter,
-      instagram,
-      linkedin,
-      facebook,
-      // spread the rest of the fields we don't need to check
-      ...rest
-    } = req.body;
-
-    // build a profile
-    const profileFields = {
-      user: req.user.id,
-      website:
-        website && website !== ''
-          ? normalize(website, { forceHttps: true })
-          : '',
-      skills: Array.isArray(skills)
-        ? skills
-        : skills.split(',').map((skill) => ' ' + skill.trim()),
-      ...rest
-    };
-
-    // Build socialFields object
-    const socialFields = { youtube, twitter, instagram, linkedin, facebook };
-
-    // normalize social fields to ensure valid url
-    for (const [key, value] of Object.entries(socialFields)) {
-      if (value && value.length > 0)
-        socialFields[key] = normalize(value, { forceHttps: true });
-    }
-    // add to profileFields
-    profileFields.social = socialFields;
-
-    try {
-      // Using upsert option (creates new doc if no match is found):
-      let profile = await Profile.findOneAndUpdate(
-        { user: req.user.id },
-        { $set: profileFields },
-        { new: true, upsert: true, setDefaultsOnInsert: true }
-      );
-      return res.json(profile);
-    } catch (err) {
-      console.error(err.message);
-      return res.status(500).send('Server Error');
-    }
-  }
-);
 router.post('/add-profile',[auth,[check('status','status is required').not().isEmpty(),
   check('skills','skills is required').not().isEmpty()
  ]],async(request,response)=>{
@@ -85,8 +25,18 @@ router.post('/add-profile',[auth,[check('status','status is required').not().isE
    if (!errors.isEmpty()) {
   return response.status(400).json({ errors: errors.array() });
     }
-   const{ company,website,location,bio,status,githubusername,skills,youtube,facebook,twitter,instagram}=
-   request.body;
+   const{ 
+     company,
+     website,
+     location,
+     bio,
+     status,
+     githubusername,
+     skills,
+     youtube,
+     facebook,
+     twitter,
+     instagram} =  request.body;
   const profile = {};
   profile.user = request.user.id;
   if(company){
@@ -114,6 +64,7 @@ router.post('/add-profile',[auth,[check('status','status is required').not().isE
   if(youtube){
     profile.social.you
   }
+  return response.status(200).json({msg:"send"});
 });
 
 module.exports = router;
