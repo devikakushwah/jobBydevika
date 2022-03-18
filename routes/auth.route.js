126d28b6f265001944514e2561ff5e10756fbbe5
devikakushwah <95033933+devikakushwah@users.noreply.github.com>
2022-03-18 13:55:55 +0530

    user again

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,4 +1,10 @@
 const express = require('express');
+
+const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+const {check,validationResult}= require('express-validator');
 const auth = require('../middleware/auth');
 const User = require('../model/user.model');
 const router = express.Router();
@@ -12,5 +18,56 @@ router.get('/', auth, async (req, res) => {
      return  res.status(500).json({msg:'Server Error'});
     }
   });
-
+  router.post(
+    '/',
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({msg:"validation error" });
+      }
+  
+      const { email, password } = req.body;
+  
+      try {
+        let user = await User.findOne({ email });
+  
+        if (!user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+  
+        const isMatch = await bcrypt.compare(password, user.password);
+  
+        if (!isMatch) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+  
+        const payload = {
+          user: {
+            id: user.id
+          }
+        };
+  
+        jwt.sign(
+          payload,
+          config.get('jwtSecret'),
+          { expiresIn: '5 days' },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+          }
+        );
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+      }
+    }
+  );
+  
+  
 module.exports = router;
