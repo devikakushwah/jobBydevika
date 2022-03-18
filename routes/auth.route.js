e20149ffd6efb8eb98b8bf349af1dae9494d1e60
devikakushwah <95033933+devikakushwah@users.noreply.github.com>
2022-03-18 13:08:35 +0530

    user again

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,7 +3,14 @@ const auth = require('../middleware/auth');
 const router = express.Router();
 
 router.get('/',auth,(request,response)=>{
-    return response.status(200).json({msg:"auth to"});
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        return response.status(200).json({user});
+      } catch (err) {
+        console.error(err.message);
+        return response.status(500).json({msg:"error"});
+      }
+   
 })
 
 module.exports = router;
