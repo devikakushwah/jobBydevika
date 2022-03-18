1ef7528f6736bfc32b7d6a388a9165298a80f6f9
devikakushwah <95033933+devikakushwah@users.noreply.github.com>
2022-03-18 14:28:40 +0530

    user again

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router =  express.Router();
 
+const config = require('config');
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+// bring in normalize to give us a proper url, regardless of what user ente
 
-router.post('/add-profile',(request,response)=>{
+// router.post('/add-profile',(request,response)=>{
  
-});
+// });
 
-
+router.get('/me', auth, async (req, res) => {
+    try {
+      const profile = await Profile.findOne({
+        user: req.user.id
+      }).populate('user', ['name', 'avatar']);
+  
+      if (!profile) {
+        return res.status(400).json({ msg: 'There is no profile for this user' });
+      }
+  
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
 module.exports =router;
