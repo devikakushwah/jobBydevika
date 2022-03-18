5e89d55a0873207e8a0b1643144afc35cb7a97bf
devikakushwah <95033933+devikakushwah@users.noreply.github.com>
2022-03-18 15:14:18 +0530

    user again

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -10,19 +10,19 @@ const { check, validationResult } = require('express-validator');
  
 // });
 
-router.get('/me', auth, async (req, res) => {
-    try {
-      const profile = await Profile.findOne({
-        user: req.user.id
-      }).populate('user', ['name', 'avatar']);
+// router.get('/me', auth, async (req, res) => {
+//     try {
+//       const profile = await Profile.findOne({
+//         user: req.user.id
+//       }).populate('user', ['name', 'avatar']);
   
-      if (!profile) {
-        return res.status(400).json({ msg: 'There is no profile for this user' });
-      }
+//       if (!profile) {
+//         return res.status(400).json({ msg: 'There is no profile for this user' });
+//       }
   
-      return res.status(200).json(profile);
-    } catch (err) {
-      res.status(500).json({err:err.array});
-    }
-  });
+//       return res.status(200).json(profile);
+//     } catch (err) {
+//       res.status(500).json({err:err.array});
+//     }
+//   });
 module.exports =router;
